12e803c1bcd230eacbd99d6d8422a516622a6977
linnil1 <linnil1.886@gmail.com>
2018-10-09 01:04:07 +0800

    Add grafana

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -3,13 +3,13 @@ IP_ROUTER='192.168.1.2'
 IP_NAS='192.168.1.3'
 IP_HP_PRINTER='192.168.1.9'
 NAS_MONITOR_PATH='/path'
-home='\/home\/ubuntu\/LabServer_monitor'
+home='\/home\/admin\/LabServer_monitor'
 
 # replace word
 echo "Change domain name and port and sql password"
 sed -i "s/\$IP_SWITCH/$IP_SWITCH/g" monitor.yml
 sed -i "s/\$IP_ROUTER/$IP_ROUTER/g" monitor.yml
-sed -i "s/\$IP_NAS/$IP_NAS/g" monitor.yml pv.yml
+sed -i "s/\$IP_NAS/$IP_NAS/g" monitor.yml pv*.yml
 sed -i "s/\$IP_HP_PRINTER/$IP_HP_PRINTER/g" monitor.yml
 sed -i "s/\$NAS_MONITOR_PATH/$NAS_MONITOR_PATH/g" pv.yml
 sed -i "s/\~/$home/g" blackbox.yml hp_snmp.yml
@@ -21,4 +21,15 @@ docker push registry-svc.default.svc.cluster.local:5002/linnil1/nvidia_smi_expor
 docker build docker_apcupsd_exporter -t registry-svc.default.svc.cluster.local:5002/linnil1/apcupsd_exporter
 docker push registry-svc.default.svc.cluster.local:5002/linnil1/apcupsd_exporter
 
-bash rerun
+echo "start monitor pod"
+kubectl create -f apcupsd_exporter.yml -f blackbox.yml -f nvidia_smi_exporter.yml -f snmp_default.yml -f snmp_hp.yml -f snmp_router.yml
+
+mkdir ../prometheus_data
+mddir ../grafana_data
+sudo chown 472:472 ../grafana_data
+
+echo "start pv"
+kubectl create -f pv_prometheus.yml
+helm install --name lab-monitor --namespace monitor stable/prometheus --values=monitor.yml
+kubectl create -f pv_grafana.yml
+helm install --name monitor-grafana --namespace monitor stable/grafana --values=grafana.yml
