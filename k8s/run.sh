54ea1e44022430d0943be9081755e21c45bb718f
linnil1 <linnil1.886@gmail.com>
2020-07-27 10:13:21 +0800

    Update

--- k8s/run.sh
+++ k8s/run.sh
@@ -5,6 +5,6 @@ echo "start monitor pod"
 kubectl create -f apcupsd_exporter.yml -f blackbox.yml -f nvidia_exporter.yml -f snmp_default.yml -f snmp_hp.yml -f snmp_router.yml
 
 echo "start grafana and promethus"
-helm install lab-monitor --namespace monitor -f monitor.yml stable/prometheus
+helm install lab-monitor --namespace monitor -f monitor.yml --version 9.3.1 stable/prometheus
 helm install monitor-grafana --namespace monitor -f grafana.yml stable/grafana
-# helm upgrade lab-monitor -f monitor.yml
+# helm upgrade lab-monitor stable/grafana --namespace monitor -f monitor.yml
