8877e03c507adc8ebf0c4f7ddd723012a6063311
linnil1 <linnil1.886@gmail.com>
2020-08-11 20:02:06 +0800

    Fix some bugs

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@ set -xe
 
 echo "Change basic setting from config.yaml"
 docker run --rm -it -v "$PWD:/app" dcagatay/j2cli k8s/pv.yml config.yaml -o k8s/pv.yml
-docker run --rm -it -v "$PWD:/app" dcagatay/j2cli k8s/pv.yml k8s/grafana.yml config.yaml -o k8s/grafana.yml
-docker run --rm -it -v "$PWD:/app" dcagatay/j2cli k8s/pv.yml k8s/monitor.yml config.yaml -o k8s/monitor.yml
+docker run --rm -it -v "$PWD:/app" dcagatay/j2cli k8s/grafana.yml config.yaml -o k8s/grafana.yml
+docker run --rm -it -v "$PWD:/app" dcagatay/j2cli k8s/monitor.yml config.yaml -o k8s/monitor.yml
 
+cd k8s
 echo "start pv"
 kubectl create -f pv.yml
 
@@ -14,4 +15,5 @@ kubectl create -f apcupsd_exporter.yml -f blackbox.yml -f nvidia_exporter.yml -f
 echo "start grafana and promethus"
 helm install lab-monitor --namespace monitor -f monitor.yml --version 9.3.1 stable/prometheus
 helm install monitor-grafana --namespace monitor -f grafana.yml stable/grafana
+cd ..
 # helm upgrade lab-monitor stable/grafana --namespace monitor -f monitor.yml
